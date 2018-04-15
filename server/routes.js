4fdbfe43b33d31f0603f81ffd75da7f09756ac04
Dan Hisey <dchisey@gmail.com>
2018-04-14 20:18:40 -0500

    added server files

---
+++ server/routes.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const data = require('./controllers');
+const mongoose = require('mongoose');
+const WeeklyResult = require('./models');
+
+
+module.exports = function(app) {
+	app.get('/', (req, res) => {
+		res.send('sup')
+	})
+
+	app.post('/api/headtohead', (req, res) => {
+		const firstOwner = new RegExp(req.body.firstOwner, 'i');
+		const secondOwner = new RegExp(req.body.secondOwner, 'i');
+
+		// WeeklyResult.find({ $or : [{ Owner: firstOwner }, { Owner: secondOwner }] })
+		// 	.sort('Week Owner')
+		// 	.exec((err, data) => {
+		// 		if(err) console.log(err);
+
+		// 		console.log(data);
+		// 		res.json(data)
+		// 	});
+
+		WeeklyResult.find({}, (err, data) => {
+			res.send(data)
+			console.log(err)
+		})
+	});
+
+	app.post('/api/leaguecomparison', (req, res) => {
+		const week = 12;
+		WeeklyResult.aggregate([
+			{
+				$match: { Week: { $lte: week }}
+			},
+			{
+				$group: {
+					_id: '$Owner',
+					Week: { $max: '$Week' },
+					Pts: { $sum: '$Pts' },
+					Elite: { $sum: '$Elite' },
+					Superior: { $sum: '$Superior' },
+					Inferior: { $sum: '$Inferior' },
+					MeanPlus: { $sum: '$MeanPlus' },
+					MeanMinus: { $sum: '$MeanMinus' },
+					Abyssmal: { $sum: '$Abyssmal' },
+					High: { $max: '$Pts' },
+					Low: { $min: '$Pts' },
+					History: { $push: { 
+						Pts: '$Pts',
+						Elite: '$Elite',
+						Superior: '$Superior',
+						MeanPlus: '$MeanPlus',
+						MeanMinus: '$MeanMinus',
+						Inferior: '$Inferior',
+						Abyssmal: '$Abyssmal',
+						WeeklyQS: { 
+							$sum: [
+								'$Elite',
+								'$Superior',
+								'$MeanPlus',
+								'$MeanMinus',
+								'$Inferior',
+								'$Abyssmal' 
+						]}
+					 }}
+				}
+			}
+		], (err, data) => {
+			console.log(data);
+			console.log(err)
+			res.json(data);
+		});
+	});
+}
--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.ajax = this.ajax.bind(this);
+  }
+  
+  componentWillMount() {
+    this.ajax()
+  }
+
+  async ajax() {
+    const options = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" }
+    }
+
+    const uri = 'http://localhost:3000/api/leaguecomparison'
+    const data = await fetch(uri, options)
+      .then(res => res.json())
+      .catch(e => console.warn(e));
+    console.log(data)
+    return data
+  }
+
   render() {
     return (
       <h1>sup</h1>
